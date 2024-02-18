a8ed07335bf424cfe4909a328513fb8055d9e738
romyaa <raneemmahmoud111@gmail.com>
2024-02-18 22:40:33 +0200

    adding add to cart button logic

--- singleProduct.js
+++ singleProduct.js
@@ -36,7 +36,56 @@ fetch(`https://dummyjson.com/products/${productID}`)
     image4.src = data.images[3];
   });
 
+  let cart = [];
+  let counter = new Array(30);
+  counter.fill(0);
+  function initialize(cart, counter) {
+    if (localStorage.length) {
+      window.addEventListener("load", function () {
+        let fetchedItem = JSON.parse(localStorage.getItem("itemsId"));
+        let fetchedcounter = JSON.parse(localStorage.getItem("itemsCounter"));
+        for (let i = 0; i < fetchedItem.length; i++) {
+          cart.push(fetchedItem[i]);
+        }
+        for (let i = 0; i < fetchedcounter.length; i++) {
+          counter[i] = fetchedcounter[i];
+        }
+      });
+    }
+  }
+  initialize(cart, counter);
+  
+  // document.getElementById("addToCart").addEventListener("click", function () {
+  //   cart.push(add${productID - 1});
+  //   console.log(cart);
+  //   console.log(counter);
+  //   counter[productID - 1] = 1;
+  //   localStorage.setItem("itemsCounter", JSON.stringify(counter));
+  //   localStorage.setItem("itemsId", JSON.stringify(cart));
+  // });
+  let mmm=document.getElementById("addToCart");
 
+mmm.addEventListener("click", function () {
+ if(mmm.innerText==="ADD TO CART"){
+  cart.push(`add${productID-1}`)
+  counter[productID-1] = 1
+  localStorage.setItem('itemsCounter', JSON.stringify(counter))
+  localStorage.setItem('itemsId', JSON.stringify(cart))
+  mmm.innerText = "REMOVE FROM CART"
+
+}
+  else if (mmm.innerText == "REMOVE FROM CART") {
+    let thisId =(`add${productID-1}`)
+    console.log(thisId)
+    let afterDel = cart.filter(function (product) {
+        return product != thisId
+    })
+    cart = [...afterDel]
+    localStorage.setItem('itemsId', JSON.stringify(cart))
+    this.innerText = "ADD TO CART"
+
+}
+})
 
   ////////////////////////////////////////////////////////////////////
 
